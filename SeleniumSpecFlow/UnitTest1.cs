ccc29af7a96046cb68de76b86e9b1946033fb84a
Bijaya Ojha <ojhabijaya87@gmail.com>
2020-11-21 13:22:30 +0530

    Add project files.

---
+++ SeleniumSpecFlow/UnitTest1.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumSpecFlow
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://www.google.co.in";
+        }
+    }
+}
